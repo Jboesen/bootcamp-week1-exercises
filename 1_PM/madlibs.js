b67f0c60737cd5e8a64e3269a9dfeaf97c6d9dc0
John Boesen <89944638+Jboesen@users.noreply.github.com>
2022-01-04 09:42:28 -0600

    Added madlibs answers

--- 1_PM/madlibs.js
+++ 1_PM/madlibs.js
@@ -10,9 +10,19 @@ const verb1 = ______
 
 Change thses values to generate a different story
 */
+// Source: https://madlibs.bostonchildrensmuseum.org/madlibs/famouscatsform.php
 
-const story = `
-**replace this with your story using a template string**
+const adj1 = 'epic'
+const noun1 = 'Rolex'
+const num1 = '9'
+const catName = 'Bob'
+const food = 'Pad Thai'
+const verb2 = 'exist'
+const otherName = 'Spot'
+
+story = `
+Morris—the cat with the ${adj1} attitude and the posh ${noun1} is the "spokesperson" for ${num1} cat ${catName}.
+Garfield—the famous comic-stip cat who hates ${food}, loves to ${verb2}, and has no respect for ${otherName}, his owner's dog.
 `
 
 console.log(story)
