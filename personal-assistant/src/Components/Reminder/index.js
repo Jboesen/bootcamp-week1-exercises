2c76f598101069ee76fb777c078b59e2265cd26c
John Boesen <jmboesen@college.harvard.edu>
2022-01-16 14:45:47 -0600

    deleted files from reminders

--- personal-assistant/src/Components/Reminder/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Reminder } from './styles'
-
-const ReminderComponent = ({ text }) => (
-  <Reminder >{text}</Reminder>
-)
-// onClick={() => this.props.removeReminder()}
-export default ReminderComponent
